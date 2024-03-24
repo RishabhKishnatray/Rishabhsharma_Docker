8e677080f702f5565be36f8b7f7e052b1753ef36
root <root@ip-172-31-34-176.ec2.internal>
2024-03-24 15:28:24 +0000

    salaryapi and helm

---
+++ Salary_API/src/test/java/com/opstree/microservice/salary/SalaryApplicationTests.java
@@ -0,0 +1,14 @@
+package com.opstree.microservice.salary;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SalaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+		final String name = "Deepak";
+		assertEquals(name, "Deepak");
+	}
+
+}
